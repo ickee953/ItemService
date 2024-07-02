76d1ddeee8dcfe48a1d26974f7e823ca8d1a5924
ickee953 <ickee953@gmail.com>
2024-07-02 23:39:20 +0300

    Init spring boot application.

---
+++ src/main/java/com/github/ickee953/micros/items/ItemsApplication.java
@@ -0,0 +1,13 @@
+package com.github.ickee953.micros.items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ickee953/micros/items/ItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ickee953.micros.items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
